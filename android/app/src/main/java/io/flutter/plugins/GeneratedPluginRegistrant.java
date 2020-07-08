78c1e2fd9addb0f3575719b45bc651466209e3f0
Hannibal404 <hannibal404@users.noreply.github.com>
2020-07-08 19:17:38 +0530

    Added login/signup

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -13,7 +13,9 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebaseauth.FirebaseAuthPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.core.FirebaseCorePlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.googlesignin.GoogleSignInPlugin());
   }
 }
--- lib/Screens/Login/components/loginBody.dart
+++ lib/Screens/Login/components/loginBody.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Login/components/background.dart';
 import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
@@ -7,11 +9,130 @@ import 'package:flutter_auth/components/rounded_input_field.dart';
 import 'package:flutter_auth/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginBody extends StatelessWidget {
-  const LoginBody({
+enum AuthMethod {
+  SIGN_IN,
+}
+
+class AuthResponse {
+  AuthResponse({this.user, this.error});
+
+  final FirebaseUser user;
+
+  final dynamic error;
+}
+
+class AuthService {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Firestore _db = Firestore.instance;
+
+  /// Firebase user one-time fetch
+  Future<FirebaseUser> get getUser => _auth.currentUser();
+
+  /// Firebase user a realtime stream
+  Stream<FirebaseUser> get user => _auth.onAuthStateChanged;
+
+  /// Check the database to see if the user has entered VALORANT info like their Riot ID and their "main", if not, we open a form for them to enter that data.
+  ///
+  /// Returns a future that will resolve when the user has entered their data and the database has been updated.
+  /// The future will resolve instantly if the user has already entered data.
+  Future<void> openUserDataEntryFormIfNoData(
+    String uid,
+    BuildContext context,
+  ) async {
+    final userData = await _db.collection("users").document(uid).get();
+
+    if (!userData.exists) {
+      // Push them to the user info entry form where they will update the DB with their info
+      await Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => SignUpScreen(),
+        ),
+      );
+    }
+  }
+
+  /// Sign in with Google.
+  Future<AuthResponse> googleSignIn(BuildContext context) async {
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final result = await _auth.signInWithCredential(credential);
+      final user = result.user;
+
+      await openUserDataEntryFormIfNoData(user.uid, context);
+
+      return AuthResponse(user: user);
+    } on NoSuchMethodError {
+      // If the Google popup was closed, don't return an error, return null.
+      return null;
+    } catch (error) {
+      return AuthResponse(error: error);
+    }
+  }
+
+  Future<AuthResponse> emailSignIn(BuildContext context, userMap) async {
+    try {
+      if (userMap == null) {
+        return null;
+      } else {
+        final email = userMap['email'];
+        final password = userMap['password'];
+
+        if (userMap['method'] == AuthMethod.SIGN_IN) {
+          final AuthResult authResult = await _auth.signInWithEmailAndPassword(
+              email: email, password: password);
+
+          final user = authResult.user;
+
+          return AuthResponse(user: user);
+        } else {
+          final AuthResult authResult = await _auth
+              .createUserWithEmailAndPassword(email: email, password: password);
+
+          final user = authResult.user;
+
+          await openUserDataEntryFormIfNoData(user.uid, context);
+
+          return AuthResponse(user: user);
+        }
+      }
+    } catch (error) {
+      print(error);
+      return AuthResponse(error: error);
+    }
+  }
+
+  /// Sign out
+  Future<void> signOut() => _auth.signOut();
+}
+
+class LoginBody extends StatefulWidget {
+  LoginBody({
     Key key,
   }) : super(key: key);
+
+  @override
+  _LoginBodyState createState() => _LoginBodyState();
+}
+
+class _LoginBodyState extends State<LoginBody> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String _controllerEmail, _controllerPassword;
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +150,24 @@ class LoginBody extends StatelessWidget {
             SizedBox(height: size.height * 0.03),
             RoundedInputField(
               hintText: "Your Email",
-              onChanged: (value) {},
+              onChanged: (value) {
+                _controllerEmail = value;
+              },
             ),
             RoundedPasswordField(
-              onChanged: (value) {},
+              onChanged: (value) {
+                _controllerPassword = value;
+              },
             ),
             RoundedButton(
               text: "LOGIN",
-              press: () {},
+              press: () {
+                Navigator.pop(context, {
+                  "email": _controllerEmail,
+                  "password": _controllerPassword,
+                  "method": AuthMethod.SIGN_IN,
+                });
+              },
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
@@ -56,4 +187,20 @@ class LoginBody extends StatelessWidget {
       ),
     );
   }
+
+  // Future<FirebaseUser> _handleSignIn() async {
+  //   final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+  //   final GoogleSignInAuthentication googleAuth =
+  //       await googleUser.authentication;
+
+  //   final AuthCredential credential = GoogleAuthProvider.getCredential(
+  //     accessToken: googleAuth.accessToken,
+  //     idToken: googleAuth.idToken,
+  //   );
+
+  //   final FirebaseUser user =
+  //       (await _auth.signInWithCredential(credential)).user;
+  //   print("signed in " + user.displayName);
+  //   return user;
+  // }
 }
