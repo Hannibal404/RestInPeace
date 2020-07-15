85e2c62fd9c90dad38d8e60e46a7c64b7821aaec
ADITYA KUMAR <61428235+inductor69@users.noreply.github.com>
2020-07-15 15:11:07 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/example/yogaguru/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.yogaguru;
+
+import org.tensorflow.lite.Interpreter;
+import android.os.Bundle;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.lang.Integer;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import android.content.res.AssetFileDescriptor;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+	private static final String CHANNEL = "ondeviceML";
+	protected Interpreter tflite;
+
+		@Override
+		 public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+		GeneratedPluginRegistrant.registerWith(flutterEngine);
+		new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(new MethodCallHandler() {
+			@Override
+			public void onMethodCall(MethodCall call, Result result) {
+				if (call.method.equals("predictData")) {
+
+					try {
+						tflite = new Interpreter(loadModelFile(call.argument("model") + ".tflite"));
+					} catch (Exception e) {
+						System.out.println("Exception while loading: " + e);
+						throw new RuntimeException(e);
+					}
+					ArrayList<Double> args = call.argument("arg");
+					float prediction = predictData(args);
+					if (prediction != 0) {
+						result.success(prediction);
+					} else {
+						result.error("UNAVAILABLE", "prediction  not available.", null);
+					}
+				} else {
+					result.notImplemented();
+				}
+			}
+		});
+		 }
+	
+
+	// This method interact with our model and makes prediction returning value of
+	float predictData(ArrayList<Double> input_data) {
+		float inputArray[][] = new float[1][input_data.size()];
+		int i = 0;
+		for (Double e : input_data) {
+			inputArray[0][i] = e.floatValue();
+			i++;
+		}
+		float[][] output_data = new float[1][1];
+		tflite.run(inputArray, output_data);
+
+		Log.d("tag", ">> " + output_data[0][0]);
+
+		return output_data[0][0];
+	}
+
+	// method to load tflite file from device
+	private MappedByteBuffer loadModelFile(String modelName) throws Exception {
+		AssetFileDescriptor fileDescriptor = this.getAssets().openFd(modelName);
+		FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+		FileChannel fileChannel = inputStream.getChannel();
+		long startOffset = fileDescriptor.getStartOffset();
+		long declaredLength = fileDescriptor.getDeclaredLength();
+		return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+	}
+}
--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -15,19 +15,12 @@ import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
     ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
-    flutterEngine.getPlugins().add(new com.github.florent37.assets_audio_player.AssetsAudioPlayerPlugin());
     flutterEngine.getPlugins().add(new bz.rxla.audioplayer.AudioplayerPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.camera.CameraPlugin());
-    flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin());
-    flutterEngine.getPlugins().add(new io.flutter.plugins.firebaseauth.FirebaseAuthPlugin());
-    flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.core.FirebaseCorePlugin());
       io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin.registerWith(shimPluginRegistry.registrarFor("io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin"));
-    flutterEngine.getPlugins().add(new io.flutter.plugins.googlesignin.GoogleSignInPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.imagepicker.ImagePickerPlugin());
     flutterEngine.getPlugins().add(new xyz.erick.ocarina.OcarinaPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
-    flutterEngine.getPlugins().add(new com.baseflow.permissionhandler.PermissionHandlerPlugin());
-    flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
       sq.flutter.tflite.TflitePlugin.registerWith(shimPluginRegistry.registrarFor("sq.flutter.tflite.TflitePlugin"));
     flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
   }
